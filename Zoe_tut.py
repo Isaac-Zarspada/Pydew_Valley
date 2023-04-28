4824a1edace9a494773ad1a4bff0e6e30cbcba2c
Isaac-Zarspada <Isaaczarspada@gmail.com>
2023-04-28 17:44:39 -0500

    lesson 1

---
+++ Zoe_tut.py
@@ -0,0 +1,18 @@
+# so Zoe this is a tutorial for you I want you to notice the name of this file, it's called zoe_tut.py
+# the .PY part of the file name tells you what kind of file it is .py i short for python and that means the file is suppose to be read a certain way by the computer.
+
+# this line right here starts with a hashtag, in python a hastag tells the computer to ignore the text
+
+you can comment multiple lines
+at the same time by highlighting
+the stuff you want to comment out
+and then pressing ctl + / 
+
+# next we're going to talk about simple stuff like objects
+# so think of an object as a box, you can put different things in
+
+# each box is defined by one equal siogn =
+
+box = 1
+
+# in the example below I made an object and since I put a 1 inside it's a certain type of object called an integer which just means whole number
