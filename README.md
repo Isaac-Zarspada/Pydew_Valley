8a89625b5ba8d55857eab3a328f3c5a9ddca2f6f
Isaac-Zarspada <Isaaczarspada@gmail.com>
2023-04-28 18:05:49 -0500

    juinibiy

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Pydew_Valley
 making a game with my sister zoe on python using pygame and other packages!
+pau smells like fish - zoe
+aewoeufhnoajnv ojnawv
+aepp, gqr
+;lmv
+ ;ae,
+ vlmae
+ lrm
+ akll mv
+ l;am
+  ;lvmr;l me;lma
+   ;lm
--- settings.py
+++ settings.py
@@ -1,4 +1,14 @@
+from pygame.math import Vector2
+# screen 
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+TILE_SIZE = 64
 
-
-SCREEN_WIDTH = 
-SCREEN_HEIGHT = 
+# overlay positions 
+OVERLAY_POSITIONS = { 
+     'tool' : (40, SCREEN_HEIGHT - 15),
+     'seed': (70,SCREEN_HEIGHT -5)}
+PLAYER_TOOL_OFFSET = {
+    'left': Vector2(-50,40),
+    'right'
+}
