8a76f48f9816bb89534b35f7b6adb1dc48b4ca74
Isaac-Zarspada <Isaaczarspada@gmail.com>
2023-04-28 18:25:56 -0500

    hello

--- settings.py
+++ settings.py
@@ -8,7 +8,20 @@ TILE_SIZE = 64
 OVERLAY_POSITIONS = { 
      'tool' : (40, SCREEN_HEIGHT - 15),
      'seed': (70,SCREEN_HEIGHT -5)}
+
 PLAYER_TOOL_OFFSET = {
     'left': Vector2(-50,40),
     'right'
+}
+LAYERS = {
+    'water': 0,
+    'ground': 1,
+    'soil': 2,
+    'soil water': 3,
+    'rain floor': 4,
+    'house bottom': 5,
+    'ground plant': 6,
+    'main': 7,
+    'house top' : 8,
+    'fruit' : 9,
 }
