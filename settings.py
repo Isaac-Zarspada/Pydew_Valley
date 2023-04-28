82a3860a11d8f9d2660f72dfb33f7da09bdb930d
Isaac-Zarspada <Isaaczarspada@gmail.com>
2023-04-28 18:29:26 -0500

    i made player tool offset and integrated layers

--- settings.py
+++ settings.py
@@ -8,7 +8,6 @@ TILE_SIZE = 64
 OVERLAY_POSITIONS = { 
      'tool' : (40, SCREEN_HEIGHT - 15),
      'seed': (70,SCREEN_HEIGHT -5)}
-
 PLAYER_TOOL_OFFSET = {
     'left': Vector2(-50,40),
     'right'
