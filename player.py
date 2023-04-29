9c5edaf17661b072d77f044a4eb852a48474e991
Isaac-Zarspada <Isaaczarspada@gmail.com>
2023-04-28 23:05:20 -0500

    edited player movement vector

--- player.py
+++ player.py
@@ -1,10 +1,38 @@
 import pygame 
 from settings import *
 
-class player (pygame.sprite.Sprite):
+class Player(pygame.sprite.Sprite):
     def __init__(self, pos, group):
         super().__init__(group)
 
-        self.image = pygame.Surface((64,32)) 
-        self.image.fill('red')
+        #  character setup
+        self.image = pygame.Surface((32,64)) 
+        self.image.fill('black')
         self.rect = self.image.get_rect(center = pos)
+
+        # movement attributes
+        self.direction = pygame.math.Vector2()
+
+    def input(self):
+        key = pygame.key.get_pressed()
+# up and down movement
+        if key[pygame.K_UP]:
+            print('up')
+            self.direction.y = -1
+        elif key[pygame.K_DOWN]:
+            print('down')
+            self.direction.y = 1
+        else:
+            self.direction.y=0
+# left right movement
+        if key[pygame.K_LEFT]:
+            print('left')
+            self.direction.x = -1
+        elif key[pygame.K_RIGHT]:
+            print('right')
+            self.direction.x = 1
+        else:
+            self.direction.y=0
+        print(self.direction)
+    def update(self, dt):
+        self.input()
