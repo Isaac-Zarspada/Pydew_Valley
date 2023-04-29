e22d943e78759c958f9a5aba3030b9211604b797
Isaac-Zarspada <Isaaczarspada@gmail.com>
2023-04-28 22:29:21 -0500

    add a inishallised object

--- level.py
+++ level.py
@@ -2,5 +2,12 @@ import pygame
 from settings import *
 
 class Level:
+    def __init__(self):
+
+        self.display_surface = pygame.display.get_surface()
+        self.all_sprites = pygame.sprite.Group()
+
+
+
     def run(self, dt):
         print('run game')
